a79dc5018903c271d2a2f083a174801c2ca5ad77
cxtan <chenxi.tan@ingenic.com>
2016-10-12 12:35:18 +0800

    add change the current directory in jpg bmp..formats photos function

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 #!/bin/python
 # encoding: utf-8
 
-import Image
-import ImageDraw
-import ImageFont
-
-image_path = "./test.jpg"
-
-
+import Image, ImageDraw, ImageFont
+import os, sys
+import re
 class image(object):
-    def __init__(self, path):
-        self.__path = path
+    def __init__(self):
         self.__im = None
 
-    def get_path(self):
-        path = self.__path
-        return path
-
-    def open_image(self, path):
+    def open_image(self):
         try:
-            self.__im = Image.open(path)
-            print self.__im.mode,self.__im.size,self.__im.format
-            dr = ImageDraw.Draw(self.__im)
-            font = ImageFont.truetype('msyh.ttf', 60)
-            dr.text((self.__im.size[0]*0.8, self.__im.size[1]*0.05), "1", fill="#ff0000", font=font)
-            self.__im.show()
-            self.__im.save('result.jpg')
-        except IOError :
-            print "file no exist"
+            for path in os.listdir("."):
+                image_name, image_format = os.path.splitext(path)
+                if re.match( r"jpg|raw|bmp", image_format[1:]):
+                    self.__im = Image.open(path)
+                    dr = ImageDraw.Draw(self.__im)
+                    font = ImageFont.truetype('msyh.ttf', 60)
+                    dr.text((self.__im.size[0]*0.8, self.__im.size[1]*0.05), "1", fill="#ff0000", font=font)
+                    self.__im.save(image_name + "_change" + image_format)
+                else:
+                    pass
+        except IOError, e:
+            print "%s file no exist" % self.__path
 
 if __name__ == '__main__':
-    im = image(image_path)
-    im.open_image(im.get_path())
+    im = image()
+    im.open_image()
     pass
 
 
