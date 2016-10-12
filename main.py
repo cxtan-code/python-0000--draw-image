a2a207d6786585bc44c3111ce6498459220b7a15
cxtan <chenxi.tan@ingenic.com>
2016-10-12 10:35:09 +0800

    can add sign for image

--- main.py
+++ main.py
@@ -3,8 +3,11 @@
 
 import Image
 import ImageDraw
+import ImageFont
 
 image_path = "./test.jpg"
+
+
 class image(object):
     def __init__(self, path):
         self.__path = path
@@ -15,14 +18,24 @@ class image(object):
         return path
 
     def open_image(self, path):
-        self.__im = Image.open(path)
-        print self.__im.mode,self.__im.size,self.__im.format
-        self.__im.show()
+        try:
+            self.__im = Image.open(path)
+            print self.__im.mode,self.__im.size,self.__im.format
+            dr = ImageDraw.Draw(self.__im)
+            font = ImageFont.truetype('msyh.ttf', 60)
+            dr.text((self.__im.size[0]*0.8, self.__im.size[1]*0.05), "1", fill="#ff0000", font=font)
+            self.__im.show()
+            self.__im.save('result.jpg')
+        except IOError :
+            print "file no exist"
 
 if __name__ == '__main__':
     im = image(image_path)
     im.open_image(im.get_path())
     pass
+
+
+
 
 
 
